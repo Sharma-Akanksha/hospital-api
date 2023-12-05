9f016fd8184a512be5cc9eeee66db612070acdf0
Akanksha Sharma <sharma.akanksha66@gmail.com>
2023-12-05 16:38:05 +0530

    update db address

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
+module.exports = {
+    MongoURI: "mongodb://admin:admin987@cluster0-shard-00-00.r3fs6.mongodb.net:27017,cluster0-shard-00-01.r3fs6.mongodb.net:27017,cluster0-shard-00-02.r3fs6.mongodb.net:27017/auth?authSource=admin&replicaSet=atlas-638q0p-shard-0&readPreference=primary&ssl=true"
+}
 
 //connect to DB
-mongoose.connect('mongodb://localhost/hospital_API', { useNewUrlParser: true ,useUnifiedTopology: true });
+mongoose.connect(module.exports.MongoURI, { useNewUrlParser: true ,useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, "Error connecting to db"));
